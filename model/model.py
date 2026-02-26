e0902a23680ac3dbfc205e35791bd88473a2a10c
Riverxia <riverdalecoder@outlook.com>
2026-02-26 16:33:55 -0500

    (acm) Currently Listening To: I am VERY concerned for these girls... - Therapist Plays Doki Doki Literature Club - 1 - Euro Brady

--- model/model.py
+++ model/model.py
@@ -3,7 +3,4 @@ from google.genai import types
 from google.genai.types import Type
 import model.config
 conf=model.config.Config()
-client = genai.Client(
-    http_options={"api_version": "v1beta"},
-    api_key=conf.api_key
-)
+client = genai.Client(http_options={"api_version": "v1beta"}, api_key=conf.api_key)
