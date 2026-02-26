7670ede5eed334c381d8e73df8b50ae9fe235788
Riverxia <riverdalecoder@outlook.com>
2026-02-26 16:32:14 -0500

    (acm) Currently Listening To: I am VERY concerned for these girls... - Therapist Plays Doki Doki Literature Club - 1 - Euro Brady

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-def main():
-    print("Hello from aiwaifu!")
-
-
-if __name__ == "__main__":
-    main()
+import model.model
---
+++ model/model.py
@@ -0,0 +1,5 @@
+from google import genai
+from google.genai import types
+from google.genai.types import Type
+import model.config
+
